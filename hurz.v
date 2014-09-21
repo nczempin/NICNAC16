c537ea22441d78a2072a23a053ddbf73b5a0467b
Nicolai Czempin <contact@czempin.de>
2014-09-21 09:59:02 +0200

    removed leftover, misleading comments

--- hurz.v
+++ hurz.v
@@ -2,13 +2,8 @@
 module ROM (ADDRESS, VALUE);
  input  [7:0]ADDRESS; 
  output [15:0] VALUE;
- // declares a memory rom of 8 4-bit registers.
-  //The indices are 0 to 7
-  //(* synthesis, rom_block = "ROM_CELL XYZ01" *)
  reg    [15:0]rom[0:7]; 
  integer i;
- // NOTE: To infer combinational logic instead of a ROM, use
- // (* synthesis, logic_block *)
 
 initial begin
         $readmemb("../rom.txt", rom);
