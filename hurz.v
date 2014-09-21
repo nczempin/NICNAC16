924cb2065ec9e65670eec57eff6dedf54b04675e
Nicolai Czempin <contact@czempin.de>
2014-09-21 11:33:57 +0200

    fixed the ROM. TODO: automatically read ALL the values

--- hurz.v
+++ hurz.v
@@ -2,12 +2,12 @@
 module ROM (ADDRESS, VALUE);
  input  [7:0]ADDRESS; 
  output [15:0] VALUE;
- reg    [15:0]rom[0:7]; 
+ reg    [15:0]rom[0:15]; 
  integer i;
 
 initial begin
         $readmemb("../rom.txt", rom);
-              for(i = 0; i < 8; i = i + 1)
+              for(i = 0; i < 16; i = i + 1)
               $display("memory[%d] = %b", i[4:0], rom[i]);
            end
 assign VALUE = rom[ADDRESS];
