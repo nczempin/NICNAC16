8b8f383b8d4f51e8119b25334576b39b92df20ae
Nicolai Czempin <nicolai.czempin@gmail.com>
2019-08-14 21:50:47 +0200

    Fake the PC reaching a JMP instruction (just for fetch for now)

--- vivado_proj/Basys-3-GPIO.srcs/sim_1/new/tb1.v
+++ vivado_proj/Basys-3-GPIO.srcs/sim_1/new/tb1.v
@@ -5,13 +5,16 @@ reg clk, reset, enable, icynext;
 wire t0, t1, t2, t3;
 wire fetch, execute;
 wire new_cycle;
-wire[15:0] pc_out, ma_out, md_out, ac_out;
+wire[15:0] pc_out, ma_out; 
+reg [15:0] md_out;
+wire[15:0] ac_out;
 wire[3:0] ir_out;
 wire incr_pc;
 initial begin
 clk=1;
 enable = 0;
 icynext = 1;
+md_out = 0;
 
 
 	//start reset
@@ -28,9 +31,13 @@ end
 always #1 clk =~clk;
 
 always #700 enable =~ enable; 
-
+always @(pc_out)
+begin
+  if (pc_out == 7)
+     md_out = 16'b0001000000000000;
+end
 system_timing st (reset, clk, icynext, t0,t1,t2,t3, fetch, execute,new_cycle);
-datapath dp (clk, reset, fetch, execute, incr_pc, pc_out, ir_out, ma_out, md_out, ac_out);
+datapath dp (clk, reset, fetch, execute, t0, t1, t2, t3, incr_pc, pc_out, ir_out, ma_out, md_out, ac_out);
 control_unit cu( fetch, execute, incr_pc);
     
 endmodule
--- vivado_proj/Basys-3-GPIO.srcs/sources_1/new/datapath.v
+++ vivado_proj/Basys-3-GPIO.srcs/sources_1/new/datapath.v
@@ -20,28 +20,31 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module datapath(
-    input clk,
-    input reset,
-    input fetch,
-    input execute,
-    input incr_pc,
-    output reg [15:0] pc_out,
-    output reg [3:0] ir_out,
-    output reg [15:0] ma_out,
-    output reg [15:0] md_out,
-    output reg [15:0] ac_out
-    );
+module datapath(clk, reset, fetch, execute, incr_pc, t0, t1, t2, t3, pc_out, ir_out, ma_out, md_out, ac_out);
+    input clk;
+    input reset;
+    input fetch;
+    input execute;
+    input incr_pc;
+    input t0, t1, t2, t3;
     
+    output reg [15:0] pc_out;
+    output [3:0] ir_out;
+    output reg [15:0] ma_out;
+    input  [15:0] md_out;
+    output reg [15:0] ac_out;
+
+    wire EN_IR;
+    assign EN_IR = fetch & t3;
+    REG4CE IR(ir_out, clk, EN_IR, reset, md_out[15:12]);
     
     always @(posedge clk or posedge reset)
     
     if (reset) begin
        pc_out <= 0;
        ac_out<=0;
-       ir_out<=0;
        ma_out<=0;
-       md_out <=0;
+       //md_out <=0;
     end else
     if (incr_pc) begin
        pc_out <= pc_out +1;
